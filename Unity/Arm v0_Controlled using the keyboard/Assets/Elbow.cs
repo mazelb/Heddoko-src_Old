9abe60c1a938ca08b4eecb041e7330eb3430d22b
Frank <frank@frnk.ca>
2014-07-25 08:02:50 -0400

    Unity arm updates

--- Unity/Arm v0_Controlled using the keyboard/Assets/Elbow.cs
+++ Unity/Arm v0_Controlled using the keyboard/Assets/Elbow.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Elbow : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Elbow : MonoBehaviour
+{
+	public float radialVelocity;
+	public float linearVelocity;
 	
+	
+	void Start ()
+	{
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()
+	{
+		
+		// Collect data from sensors
+		// ...
+		this.radialVelocity = 50f;
+		this.linearVelocity = 10f;
+		
+		// Move according to data from sensors
+		this.Move();
+	}
 	
+	// Temporary method, testing motion stuff...
+	void Move()
+	{
+		if(Input.GetKey(KeyCode.W))
+			transform.Rotate (Vector3.forward, -radialVelocity * Time.deltaTime);
+		
+		if(Input.GetKey(KeyCode.S))
+			transform.Rotate (Vector3.forward, radialVelocity * Time.deltaTime);
+		
+		if(Input.GetKey(KeyCode.A))
+			transform.Rotate(Vector3.up, radialVelocity * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			transform.Rotate (Vector3.up, -radialVelocity * Time.deltaTime);
 	}
 }
