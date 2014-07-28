ec5a02bcaf2c8acfb222f2a48a19a9e593a43457
Frank <frank@frnk.ca>
2014-07-28 16:14:15 -0400

    Unity
    Preparing Unity code to use it with the accelerometers from a serial
    port

---
+++ Unity/Arm v1_Accelerometer/Assets/Shoulder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoulder : UpperBody
+{
+	// Use this for initialization
+	public void Start ()
+	{
+
+	}
+
+	public void FixedUpdate ()
+	{
+		
+	}
+}
---
+++ Unity/Arm v1_Accelerometer/Assets/UpperBody.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class UpperBody : MonoBehaviour, Move
+{
+	// Serial port details
+	public string serialPort = "COM3";
+	public int baudRate = 9600;
+	protected SerialPort sp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		sp = new SerialPort (serialPort, baudRate);
+		sp.Open ();
+		sp.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+		// Serial test
+		if (sp.IsOpen)
+		{
+			try
+			{
+				Debug.Log(sp.ReadByte ());
+			}
+			catch (System.Exception) {}
+		}
+	}
+}
