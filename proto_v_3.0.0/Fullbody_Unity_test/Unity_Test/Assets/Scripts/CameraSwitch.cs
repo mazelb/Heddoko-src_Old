07962971d1498d5f386f15c6284e34bca0eab9cf
chepundup <mazen.elbawab@gmail.com>
2015-04-01 14:36:31 -0400

    Added camera switch between nod and stretchsense

---
+++ proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour 
+{
+	public Camera nodCamera = null;
+	public Camera stretchCamera = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(nodCamera != null && stretchCamera != null)
+		{
+			nodCamera.enabled = false;
+			stretchCamera.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.C) && nodCamera != null && stretchCamera != null) 
+		{
+			if(nodCamera.enabled)
+			{
+				nodCamera.enabled = false;
+				stretchCamera.enabled = true;
+			}
+			else
+			{
+				nodCamera.enabled = true;
+				stretchCamera.enabled = false;
+			}
+		}
+	}
+}
