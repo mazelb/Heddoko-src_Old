46f789e4d8afb06f001eb39467f9f00508043240
Frank <frank@frnk.ca>
2015-06-01 15:01:14 -0400

    Updated StretchSensor code to read from COM port

--- proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/StretchSensors/StretchContainer.cs
+++ proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/StretchSensors/StretchContainer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.IO.Ports;
+using System.Threading;
 
 public class StretchContainer : MonoBehaviour 
 {
@@ -15,7 +16,7 @@ public class StretchContainer : MonoBehaviour
 	public int baudeRate = 115200;
 	private SerialPort mPortStream = null;
 
-    // Shortcut to apply data sets to all sensors at the same time.
+    // Shortcut to apply CSV data sets to all sensors at the same time.
     public string CSVDataSet = "default";
 
 	// Each joint can be composed of one or multiple sensors simultaneously
@@ -23,6 +24,7 @@ public class StretchContainer : MonoBehaviour
 
 	// Channel data
 	public static int[] mData;
+	private char[] dataBuffer = new char[21];
 
 	/// <summary>
 	/// Call this function to start reading data from the sensors for the joint values.
@@ -32,19 +34,25 @@ public class StretchContainer : MonoBehaviour
 		// Open COM port
 		if (usingCOMPort && !String.IsNullOrEmpty(COMPort))
 		{
+		    print("Using COM port: "+ COMPort);
 			mPortStream = new SerialPort(COMPort, baudeRate);
+			if (mPortStream.IsOpen) mPortStream.Close();
 			mPortStream.DataBits = 8;
 			mPortStream.StopBits = StopBits.One;
-			if (mPortStream.IsOpen) mPortStream.Close();
 			
 			// Try to open COM port and send start command
 			try {
 				mPortStream.Open();
 				mPortStream.Write("#s\r\n");
 			}
-			catch (Exception error) {}
+			catch (Exception) {}
+			print(mPortStream.IsOpen ? "Successfully connected." : "Could not open COM port.");
+
+		    print("Testing COM port.");
+		    mPortStream.Write("Testing COM port.\r\n");
 		}
 
+		// Loop through joint scripts to initialize them.
 		for (int ndx = 0; ndx < mStretchJoints.Length; ndx++) 
 		{
 			if(!mStretchJoints[ndx].independentUpdate)
@@ -60,12 +68,6 @@ public class StretchContainer : MonoBehaviour
 	/// </summary>
 	public void UpdateJoints () 
 	{
-		// test
-		if (usingCOMPort && !String.IsNullOrEmpty(COMPort)) {
-			
-			print (mPortStream.ReadLine());
-		}
-
 		for (int ndx = 0; ndx < mStretchJoints.Length; ndx++) 
 		{
 			if(!mStretchJoints[ndx].independentUpdate)
@@ -85,6 +87,31 @@ public class StretchContainer : MonoBehaviour
 			if(!mStretchJoints[ndx].independentUpdate)
 			{
 				mStretchJoints[ndx].ResetJoint();
+			}
+		}
+	}
+
+	public void ReadCOMPort()
+	{
+		// Update channel data
+		if (mPortStream.IsOpen)
+		{
+		    print("Reading line...");
+		    string rawData = mPortStream.ReadLine();
+//		    string rawData = mPortStream.ReadTo("\r");
+//		    Int32 num = mPortStream.Read(dataBuffer, 0, 21);
+//		    string rawData = new string(dataBuffer);
+			print (rawData);
+			if (rawData.Length >= 21 && rawData[0] == '!')
+			{
+			    mData[1] = Convert.ToInt32(rawData.Substring(1, 4));
+			    mData[2] = Convert.ToInt32(rawData.Substring(5, 4));
+			    mData[3] = Convert.ToInt32(rawData.Substring(9, 4));
+			    mData[4] = Convert.ToInt32(rawData.Substring(13, 4));
+			    mData[5] = Convert.ToInt32(rawData.Substring(17, 4));
+			}
+			else {
+			    print("Invalid data.");
 			}
 		}
 	}
@@ -116,6 +143,10 @@ public class StretchContainer : MonoBehaviour
 	/// </summary>
 	void Update() 
 	{
+	    if (usingCOMPort) {
+	        ReadCOMPort();
+	    }
+
 		UpdateJoints();
 	}
 	
