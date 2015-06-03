f82f8ec513e68e9611c06b11954a42c83eb4ae7d
Frank <frank@frnk.ca>
2015-06-03 16:10:09 -0400

    Added script for knee joint

--- proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/StretchSensors/StretchJointKnee.cs
+++ proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/StretchSensors/StretchJointKnee.cs
@@ -9,43 +9,24 @@ public class StretchJointKnee : StretchJoint
     public override void UpdateJoint()
     {
 	    // Don't update anything if joint is paused.
-	    if (pauseAnimation) {
+	    if (pauseAnimation)
+	    {
 	        return;
 	    }
 
         // Update individual sensors.
-		for (int ndx = 0; ndx < mStretchSensors.Length; ndx++) {
+		for (int ndx = 0; ndx < mStretchSensors.Length; ndx++)
+		{
 			mStretchSensors[ndx].UpdateSensor();
 		}
 
-        // Retrieve sensor objects.
-        StretchSensor elbow = getSensorByName("ssElbow");
-        StretchSensor forearm = getSensorByName("ssForearm");
-
-        if( elbow != null && forearm != null )
+        // Update knee angle
+        StretchSensor knee = mStretchSensors[0];
+        if (knee != null)
         {
-            float elbowAngle = elbow.getCurAngleReading();
-            float forearmAngle = forearm.getCurAngleReading();
-
-            mCurJointRotationEuler.y = elbowAngle * rotatesXYZ.y;
-            mCurJointRotationEuler.x = forearmAngle * rotatesXYZ.x;
-
-            jointTransform.localRotation = Quaternion.Euler(mCurJointRotationEuler);
+			mCurJointRotationEuler = knee.getCurAngleReading() * rotatesXYZ;
+			jointTransform.localRotation = Quaternion.Euler(mCurJointRotationEuler);
         }
     }
-
-    // Retrieves a sensor using the "stretchName" property.
-	protected override StretchSensor getSensorByName(String vName)
-	{
-		for (int ndx = 0; ndx < mStretchSensors.Length; ndx++)
-		{
-			if(mStretchSensors[ndx].stretchName == vName)
-			{
-				return mStretchSensors[ndx];
-			}
-		}
-
-		return null;
-	}
 }
 
