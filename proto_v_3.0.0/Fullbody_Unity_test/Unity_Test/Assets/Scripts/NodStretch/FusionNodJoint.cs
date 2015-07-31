36c8df8ef0518de672c4fd8127ef630569545fea
Javad Soltani Rad <j.soltanirad@gmail.com>
2015-07-31 09:42:21 -0400

    Added Fusion Scene.

--- proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/NodStretch/FusionNodJoint.cs
+++ proto_v_3.0.0/Fullbody_Unity_test/Unity_Test/Assets/Scripts/NodStretch/FusionNodJoint.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using Nod;
 
-public class NodJoint : MonoBehaviour  
+public class FusionNodJoint : MonoBehaviour  
 {
 
 	public NodSensor [] mNodSensors;
@@ -140,162 +140,6 @@ public class NodJoint : MonoBehaviour
 
 	}
 
-
-	//////////////////////////// These are the reference functions for matrix calculation and transformation ///////////////////////////////
-
-	/**
-	//	* RotationGlobal()
-	//	* @ This Performs transformation From Global Coordinate System To local Nod coordinates
-	//	* @params yaw pitch and roll of each nod
-	//	* @return 3*3 orientation matrix in nod local coordinate system
-	//	*/// 
-	public float[,] RotationGlobal(float yaw, float pitch, float roll)
-	{
-		float [,] a = new float[3,3];
-		a[0,0] = Mathf.Cos(pitch) * Mathf.Cos(yaw);
-		a[1,0] = (Mathf.Sin(roll) * Mathf.Cos(yaw) * Mathf.Sin(pitch) - Mathf.Sin(yaw) * Mathf.Cos(roll));
-		a[2,0] = (Mathf.Sin(roll) *Mathf.Sin(yaw) + Mathf.Cos(yaw) *Mathf.Sin(pitch)*Mathf.Cos(roll));
-		a[0,1] = Mathf.Cos(pitch)*Mathf.Sin(yaw);
-		a[1,1] = Mathf.Sin(roll)*Mathf.Sin(yaw)*Mathf.Sin(pitch) + Mathf.Cos(yaw) * Mathf.Cos(roll);
-		a[2,1] = (Mathf.Cos(roll)*Mathf.Sin(yaw)*Mathf.Sin(pitch) - Mathf.Cos(yaw) * Mathf.Sin(roll));
-		a[0,2] = -Mathf.Sin(pitch);
-		a[1,2] = Mathf.Cos(pitch)*Mathf.Sin(roll);
-		a[2,2] = (Mathf.Cos(pitch)*Mathf.Cos(roll));
-		return a;
-	}
-
-
-
-	/**
-	//	* RotationLocal()
-	//	* @ This Performs transformation From Nods Local Coordinate System To global coordinates
-	//	* @params yaw pitch and roll of each nod
-	//	* @return 3*3 orientation matrix in global coordinate system
-	//	*/// 
-	public float[,] RotationLocal( float yaw, float pitch, float roll)
-	{
-		float [,] a = new float[3,3];
-		a[0,0] = Mathf.Cos(pitch) * Mathf.Cos(yaw);
-		a[1,0] = Mathf.Cos(pitch) * Mathf.Sin(yaw);
-		a[2,0] = -Mathf.Sin(pitch);
-		a[0,1] = -Mathf.Cos(roll) *Mathf.Sin(yaw) + Mathf.Cos(yaw) *Mathf.Sin(pitch)*Mathf.Sin(roll);
-		a[1,1] = Mathf.Sin(roll)*Mathf.Sin(yaw)*Mathf.Sin(pitch) + Mathf.Cos(yaw) * Mathf.Cos(roll);
-		a[2,1] = Mathf.Cos(pitch)*Mathf.Sin(roll);
-		a[0,2] = (Mathf.Sin(roll) *Mathf.Sin(yaw) + Mathf.Cos(yaw) *Mathf.Sin(pitch)*Mathf.Cos(roll));
-		a[1,2] = (Mathf.Cos(roll)*Mathf.Sin(yaw)*Mathf.Sin(pitch) - Mathf.Cos(yaw) * Mathf.Sin(roll));
-		a[2,2] = (Mathf.Cos(pitch)*Mathf.Cos(roll));
-		return a;
-	}
-
-
-
-
-	//	/**
-	//* MatToQuat
-	//* @It converts a 3*3 orientation Matrix to a Quatrenion
-	//* @param float m[][3] is the original 3*3 matrix
-	//* @return NodQuaternionOrientation, the orientation in quatrenion
-	//* reference: @http://www.cg.info.hiroshima-cu.ac.jp/~miyazaki/knowledge/teche52.html
-	//*
-	public static NodQuaternionOrientation MatToQuat(float [,] m)
-	{
-		NodQuaternionOrientation q;
-		q.w = (m[0,0] + m[1,1] + m[2,2] + 1.0f) / 4.0f;
-		q.x = (m[0,0] - m[1,1] - m[2,2] + 1.0f) / 4.0f;
-		q.y = (-m[0,0] + m[1,1] - m[2,2] + 1.0f) / 4.0f;
-		q.z = (-m[0,0] - m[1,1] + m[2,2] + 1.0f) / 4.0f;
-		if (q.w < 0.0f) q.w = 0.0f;
-		if (q.x < 0.0f) q.x = 0.0f;
-		if (q.y < 0.0f) q.y = 0.0f;
-		if (q.z < 0.0f) q.z = 0.0f;
-		q.w = (float)Math.Sqrt(q.w);
-		q.x = (float)Math.Sqrt(q.x);
-		q.y = (float)Math.Sqrt(q.y);
-		q.z = (float)Math.Sqrt(q.z);
-		if (q.w >= q.x && q.w >= q.y && q.w >= q.z) {
-			q.w *= +1.0f;
-			q.x *= Mathf.Sign(m[2,1] - m[1,2]);
-			q.y *= Mathf.Sign(m[0,2] - m[2,0]);
-			q.z *= Mathf.Sign(m[1,0] - m[0,1]);
-		}
-		else if (q.x >= q.w && q.x >= q.y && q.x >= q.z) {
-			q.w *= Mathf.Sign(m[2,1] - m[1,2]);
-			q.x *= +1.0f;
-			q.y *= Mathf.Sign(m[1,0] + m[0,1]);
-			q.z *= Mathf.Sign(m[0,2] + m[2,0]);
-		}
-		else if (q.y >= q.w && q.y >= q.x && q.y >= q.z) {
-			q.w *= Mathf.Sign(m[0,2] - m[2,0]);
-			q.x *= Mathf.Sign(m[1,0] + m[0,1]);
-			q.y *= +1.0f;
-			q.z *= Mathf.Sign(m[2,1] + m[1,2]);
-		}
-		else if (q.z >= q.w && q.z >= q.x && q.z >= q.y) {
-			q.w *= Mathf.Sign(m[1,0] - m[0,1]);
-			q.x *= Mathf.Sign(m[2,0] + m[0,2]);
-			q.y *= Mathf.Sign(m[2,1] + m[1,2]);
-			q.z *= +1.0f;
-		}
-
-		float r = (float)Math.Sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
-		q.w /= r;
-		q.x /= r;
-		q.y /= r;
-		q.z /= r;
-		return q;
-	}
-
-
-
-
-	//
-	//	/**
-	//* RotationVector()
-	//* @It producess a rotation matrix around an arbitary vector with desired angles
-	//* @param vec u, arbitary unit vector
-	//* @param flaot t, desired angle of rotation
-	//* @return float a[][3], The output rotation matrix
-	//*/
-	public float[,] RVector(Vector3 u, float t)
-	{
-		float [,] a = new float[3,3];
-		a[0,0] = Mathf.Cos(t) + u.x*u.x* (1 - Mathf.Cos(t));
-		a[1,0] = u.x*u.y* (1 - Mathf.Cos(t)) + u.z * Mathf.Sin(t);
-		a[2,0] = u.x*u.z* (1 - Mathf.Cos(t)) - u.y * Mathf.Sin(t);
-		a[0,1] = u.x*u.y* (1 - Mathf.Cos(t)) - u.z * Mathf.Sin(t);
-		a[1,1] = Mathf.Cos(t) + u.y*u.y* (1 - Mathf.Cos(t));;
-		a[2,1] = u.z*u.y* (1 - Mathf.Cos(t)) + u.x * Mathf.Sin(t);
-		a[0,2] = u.x*u.z* (1 - Mathf.Cos(t)) + u.y * Mathf.Sin(t);
-		a[1,2] = u.z*u.y* (1 - Mathf.Cos(t)) - u.x * Mathf.Sin(t);
-		a[2,2] = Mathf.Cos(t) + u.z*u.z* (1 - Mathf.Cos(t));
-		return a;
-	}
-
-
-
-	//		/**
-	//	* multi()
-	//	*	@This Fuction do multiplication between two 3*3 matrices
-	//	*	@param matrix a and b
-	//	*	@return c = a * b,
-	//	*/
-	public float[,] multi(float[,] a, float[,] b)
-	{
-		float [,] c = new float[3,3];
-		int i, j, k;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				c[i,j] = a[i,0] * b[0,j] + a[i,1] * b[1,j] + a[i,2] * b[2,j];
-
-			}
-		}
-		return c;
-	}
-
-
-
 	/////////////////////////////////////////////////////////////////////////////////////
 	/// UNITY GENERATED FUNCTIONS 
 	//////////////////////////////////////////////////////////////////////////////////////
@@ -349,7 +193,6 @@ public class NodJoint : MonoBehaviour
 
 
 		OnGUIAngles ();
-
 		foreach (NodSensor sensor in mNodSensors) 
 		{
 			string msg = sensor.nodName;
