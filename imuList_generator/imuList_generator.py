382b42320aa02a83529d40be4e4ff96dbed3f8e3
JunaAwas <anuj68@gmail.com>
2016-03-15 15:46:55 -0400

    removed need for writing file extension

--- imuList_generator/imuList_generator.py
+++ imuList_generator/imuList_generator.py
@@ -9,11 +9,12 @@ root.withdraw()
 fn = filedialog.askopenfilename()
 
 
-fo=input('output file name: ')
+fo=input('output file name (no extension): ')
+fo+='.dat'
 
 f = open(fo,'w')
-IMUlist = np.genfromtxt (fn, delimiter=",", dtype=str)
-set=IMUlist[0][3][8::]
+nodlist = np.genfromtxt (fn, delimiter=",", dtype=str)
+set=nodlist[0][3][8::]
 
 if int(set) < 10:
     f.write('Set000')
@@ -27,39 +28,39 @@ f.write(' ,9, FFFFFFFFFF , \n')
 
 
 f.write('0,0,')
-f.write(IMUlist[0][1])
+f.write(nodlist[0][1])
 f.write('\n')
 
 f.write('0,1,')
-f.write(IMUlist[1][1])
+f.write(nodlist[1][1])
 f.write('\n')
 
 f.write('0,2,')
-f.write(IMUlist[2][1])
+f.write(nodlist[2][1])
 f.write('\n')
 
 f.write('1,3,')
-f.write(IMUlist[3][1])
+f.write(nodlist[3][1])
 f.write('\n')
 
 f.write('1,4,')
-f.write(IMUlist[4][1])
+f.write(nodlist[4][1])
 f.write('\n')
 
 f.write('1,5,')
-f.write(IMUlist[5][1])
+f.write(nodlist[5][1])
 f.write('\n')
 
 f.write('2,6,')
-f.write(IMUlist[6][1])
+f.write(nodlist[6][1])
 f.write('\n')
 
 f.write('2,7,')
-f.write(IMUlist[7][1])
+f.write(nodlist[7][1])
 f.write('\n')
 
 f.write('2,8,')
-f.write(IMUlist[8][1])
+f.write(nodlist[8][1])
 f.write('\n')
 
 f.close()
